15baf8f31b038696e11635f3b3764b9095275fd9
Saul1317 <saul.delgado.14.s@utzmg.edu.mx>
2018-01-22 13:47:57 -0600

    Primera ventana donde se elegira una sucursal

--- app/src/main/java/com/example/eduardoalejandro/encuesta/LoginActivity.java
+++ app/src/main/java/com/example/eduardoalejandro/encuesta/LoginActivity.java
@@ -8,28 +8,47 @@ import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+public class LoginActivity extends AppCompatActivity {
 
-    Spinner spinner;
+    Spinner spinner_sucursal, spinner_local;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        spinner = (Spinner) findViewById(R.id.spinner_sucursal);
+
+        spinner_sucursal = (Spinner) findViewById(R.id.spinner_sucursal);
+        spinner_local = (Spinner) findViewById(R.id.spinner_local);
+
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.spinner_sucursal, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
-    }
+        spinner_sucursal.setAdapter(adapter);
+        spinner_local.setAdapter(adapter);
 
-    @Override
-    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-        String text = spinner.getSelectedItem().toString();
-        Toast.makeText(LoginActivity.this, text , Toast.LENGTH_SHORT).show();
-    }
+        spinner_sucursal.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String text = spinner_sucursal.getSelectedItem().toString();
+                Toast.makeText(LoginActivity.this, text , Toast.LENGTH_SHORT).show();
+            }
 
-    @Override
-    public void onNothingSelected(AdapterView<?> adapterView) {
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
 
+            }
+        });
+
+        spinner_local.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String text = spinner_sucursal.getSelectedItem().toString();
+                Toast.makeText(LoginActivity.this, text , Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
     }
 }
